3bf2aad62b28b9cf6e3427da6f69e4b31fe77a94
Gurbanmyrat Ataballyyev <ataballyyev0@gmail.com>
2025-10-31 16:37:16 +0500

    update: PostHog integration removed

--- lib/src/core/kinestex_initializer.dart
+++ lib/src/core/kinestex_initializer.dart
@@ -1,4 +1,3 @@
-import 'package:posthog_flutter/posthog_flutter.dart';
 import 'generic_web_view.dart';
 import 'kinestex_logger.dart';
 
@@ -7,7 +6,6 @@ class KinesteXInitializer {
   final _logger = KinesteXLogger.instance;
 
   bool get isInitialized => _isInitialized;
-  Posthog? _posthog;
 
   Future<void> initialize(
       String apiKey, String companyName, String userId) async {
@@ -19,7 +17,6 @@ class KinesteXInitializer {
     try {
       _logger.info('Initializing SDK...');
       await GenericWebView.warmup();
-      await _initPosthog(apiKey, companyName, userId);
       _isInitialized = true;
       _logger.success('SDK initialized successfully');
     } catch (e) {
@@ -32,31 +29,6 @@ class KinesteXInitializer {
       await GenericWebView.disposeWarmup();
     } catch (e) {
       _logger.error('Dispose error', e);
-    }
-  }
-
-  Future<void> _initPosthog(
-    String apiKey,
-    String companyName,
-    String userId,
-  ) async {
-    try {
-      final config =
-          PostHogConfig('phc_5nMm5TFaJUGBdNMyPy66sNnWVjMq8y2smZjCC0ykDTR')
-            ..host = 'https://us.i.posthog.com';
-
-      await Posthog().setup(config);
-      _posthog = Posthog();
-      await _posthog?.identify(userId: userId, userProperties: {
-        'company': companyName,
-      });
-      await _posthog?.capture(
-        eventName: 'member_registered',
-        properties: {'company': companyName, 'user_id': userId},
-      );
-      _logger.success('Member added!');
-    } catch (e) {
-      _logger.error('PostHog tracking failed', e);
     }
   }
 }
